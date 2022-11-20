6b524d8b65f70156db072854269c092d4ba2f143
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-20 13:35:05 +0530

    Created a constructor for books and a container for all the books

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+let myLibrary = [];
+
+function Book(name,author,title,pages){
+    this.name = name;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;   
+}
+
+function addBookToLibrary(){
+    
+}
