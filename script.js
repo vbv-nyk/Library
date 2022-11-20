4172bc10133adf7481efd24ecbc24c53c7bf81fc
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-20 20:30:02 +0530

    Added script to add new books

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const authorName = document.querySelector("#author-name");
 const titleName = document.querySelector("#book-title");
 const pageNumber = document.querySelector("#pages-no");
 const bookList = document.querySelector(".book-list")
+const newBookContainer = document.querySelector(".new-book-container");
+const newBookButton = document.querySelector(".new-book");
 
 let myLibrary = [];
 let cur = 0;
+
+function newBook(){
+    console.log("Hi");
+    newBookContainer.style.display = "flex";
+}
 
 function Book(author,title,pages){
     this.author = author;
@@ -24,9 +31,12 @@ function displayBook(b){
     const bookAuthor = document.createElement("div");
     const bookTitle = document.createElement("div");
     const bookPages = document.createElement("div");
+    const bookNumber = document.createElement("div");
+    bookNumber.insertAdjacentText("afterbegin",`Book ${cur + 1}`);
     bookAuthor.insertAdjacentText("afterbegin",`Author: ${b.author}`);
     bookTitle.insertAdjacentText("afterbegin",`Title: ${b.title}`);
     bookPages.insertAdjacentText("afterbegin",`Pages: ${b.pages}`);
+    bookCard.appendChild(bookNumber);
     bookCard.appendChild(bookAuthor);
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookPages);
@@ -39,6 +49,7 @@ function addBookToLibrary(){
     myLibrary.push(b);
     clearForm();
     displayBook(b);
+    newBookContainer.style.display = "none";
     cur++;
 }
 
