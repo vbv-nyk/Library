9fbd277857fce3c25377e240e22b9b94fe9522cd
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-11-20 16:55:29 +0530

    Added logic to added books to an array using constructors

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 'use strict';
+const authorName = document.querySelector("#author-name");
+const titleName = document.querySelector("#book-title");
+const pageNumber = document.querySelector("#pages-no");
+
 let myLibrary = [];
 
-function Book(name,author,title,pages){
-    this.name = name;
+function Book(author,title,pages){
     this.author = author;
     this.title = title;
     this.pages = pages;   
 }
 
 function addBookToLibrary(){
-    
+    const b = new Book(authorName.value,titleName.value,pageNumber.value);
+    authorName.value = "";
+    titleName.value = "";
+    pageNumber.value = "";
+    myLibrary.push(b);
 }
